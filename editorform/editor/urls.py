ea13db4ccf73e972f59450c171b52edd455693c4
Dmitry Dmitrienko <dmitry.dmitrienko@outlook.com>
2014-01-16 21:44:58 +0400

    Add url file form application

---
+++ editorform/editor/urls.py
@@ -0,0 +1,13 @@
+#coding=UTF-8
+__author__ = "Dmitry Dmitrienko"
+__copyright__ = "Copyright 2014(c), Dmitry Dmitrienko"
+__license__ = "GPL"
+__version__ = "0.9"
+__maintainer__ = "Dmitry Dmitrienko"
+__email__ = "dmitry.dmitrienko@outlook.com"
+
+from django.conf.urls import patterns, url
+
+url_editor = patterns('',
+                      url(r'^$', '', name="home"),
+)
